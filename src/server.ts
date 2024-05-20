dcb7121fc301363d87ddf32ff90f87123e8bb77a
guusfelix2015 <gusfelix2015@gmail.com>
2024-05-19 22:32:46 -0300

    feat: create error middleware

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import express from "express";
 import cors from "cors";
 import { AppDataSource } from "./database/dataSource";
 import { routers } from "./app/routes";
+import { httpErrorMiddleware } from "./app/middlewares/ErrorMiddeware";
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routers);
+
+app.use(httpErrorMiddleware);
 
 AppDataSource.initialize().then(async () => {
   console.log("Database connected");
