3fd5f16b47d70ec00e26f4dfcdaf8cec5c534b7a
guusfelix2015 <gusfelix2015@gmail.com>
2024-05-19 22:46:41 -0300

    feat: create route

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -11,11 +11,18 @@ export class UserController {
 
   private initializeRoutes() {
     this.router.get("/", this.getAllUsers);
+    this.router.post("/", this.createUser);
   }
 
   private async getAllUsers(req: Request, res: Response) {
     const user = await UserRepository.getUsers();
     res.status(200).json(user);
+  }
+
+  private async createUser(req: Request, res: Response) {
+    const { body } = req;
+    const userCreated = await UserRepository.newUser(body);
+    return res.status(201).json(userCreated);
   }
 }
 
--- src/app/repositories/UserRepository.ts
+++ src/app/repositories/UserRepository.ts
@@ -1,10 +1,16 @@
 import { User } from "../entities/User";
 import { AppDataSource } from "../../database/dataSource";
+import { IUserInput, IUserOutput } from "../interfaces/IUser";
 
 export class UserRepository {
   private static usersRepository = AppDataSource.getRepository(User);
 
   static async getUsers() {
     return this.usersRepository.find();
+  }
+
+  static async newUser(user: IUserInput): Promise<IUserOutput> {
+    const createdUser = await this.usersRepository.save(user);
+    return createdUser;
   }
 }
