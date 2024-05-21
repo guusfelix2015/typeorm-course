27b2abcf6b7f6ebcf71f042797939642de99ddad
guusfelix2015 <gusfelix2015@gmail.com>
2024-05-21 15:28:02 -0300

    feat: configure jwtToken

---
+++ src/app/interfaces/ILogin.ts
@@ -0,0 +1,9 @@
+export interface ILogin {
+  email: string;
+  password: string;
+}
+
+export interface ITokenData {
+  name: string;
+  email: string;
+}
---
+++ src/app/utils/validations/Auth.ts
@@ -0,0 +1,38 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+import dotenv from "dotenv";
+import { ITokenData } from "../../interfaces/ILogin";
+import { ErrorExtension } from "../ErrorExtension";
+
+dotenv.config();
+
+const secret = process.env.JWT_SECRET as string;
+
+const jwtDefaultConfig: SignOptions = {
+  algorithm: "HS256",
+  expiresIn: "1h",
+};
+
+export class Auth {
+  constructor(private jwtConfig?: SignOptions) {
+    if (!jwtConfig) {
+      this.jwtConfig = jwtDefaultConfig;
+    }
+  }
+
+  public jwtGenerator(payload: ITokenData) {
+    return jwt.sign(payload, secret, this.jwtConfig);
+  }
+
+  public authenticationToken(token: string) {
+    if (!token) {
+      throw new ErrorExtension("Token not found", 401);
+    }
+
+    try {
+      const validateJwt = jwt.verify(token, secret, this.jwtConfig);
+      return validateJwt;
+    } catch (error) {
+      throw new ErrorExtension("Invalid token", 401);
+    }
+  }
+}
